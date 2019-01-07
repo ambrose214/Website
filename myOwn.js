0232533f58f9a10a7eb1930a8a7b8cce939e0273
ambrose214 <ambrose.kuo@hotmail.com>
2019-01-06 19:07:23 -0500

    commit init website

---
+++ myOwn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './myOwn.css';
+
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
